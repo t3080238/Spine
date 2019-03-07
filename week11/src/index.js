64c3b2289db438ef27e169a790f5f2de9edf0f06
kent_shi <kent_shi@mail.chungyo.net>
2019-03-07 18:09:32 +0800

    3/7

--- week11/src/index.js
+++ week11/src/index.js
@@ -59,24 +59,82 @@ window.onload = function () {
         // play animation
         symbolSpine.state.setAnimation(0, 'run', true);*/
 
+
         app.stage.on('pointerdown', function () {
-            let entry = symbolSpine.state.setAnimation(0, 'jump', false);
+
+            let entry6 = symbolSpine.state.setEmptyAnimation(1, 0);
+            entry6.mixDuration = 0.666666;
+            console.log('TCL: initial -> entry6', entry6)
+
+            let entry7 = symbolSpine.state.addAnimation(1, 'aim', false);
+
+            console.log('TCL: initial -> entry7', entry7)
+            entry7.mixDuration = 0.333333;
+
+            //entry6.alpha=0.5;
+
+            symbolSpine.state.setEmptyAnimation(2, 0);
+
+            symbolSpine.state.addAnimation(2, 'shoot', false, 0);
+
+            symbolSpine.state.addEmptyAnimation(2, 0, 0);
+
+            //console.log('TCL: initial -> entry6', entry6)
+            console.log('TCL: initial -> entry.animation', entry.animation)
+            //entry.timeScale = 2;
+
+            //let entry2 = symbolSpine.state.addAnimation(0, 'jump', false, 0);
+
+            //symbolSpine.state.setEmptyAnimation(0, 1.5);
+
+            //let entry5 = symbolSpine.state.addEmptyAnimation(1, 0, 0);
+            //console.log(entry.animation);
             //entry.alpha=0.1;
             //entry.mixDuration = 0.1;
             //symbolSpine.state.addAnimation(0, 'walk', true, 0);
             //symbolSpine.state.setAnimation(0, 'run-to-idle', false);
-             symbolSpine.state.addAnimation(0, 'run', true, 0);
+            //let entry4 = symbolSpine.state.addAnimation(0, 'run', true, 0);
+            //console.log('TCL: initial -> entry4', entry4)
+            //entry4.mixDuration=2;
         });
+
+        //symbolSpine.tint = 0xffdddd
 
         symbolSpine.stateData.setMix('run', 'run-to-idle', 0.2); //設定動作混接的時間
         symbolSpine.stateData.setMix('run-to-idle', 'run', 0.1); //設定動作混接的時間
         symbolSpine.stateData.setMix('run', 'jump', 0.2); //設定動作混接的時間
         symbolSpine.stateData.setMix('jump', 'run', 0.4); //設定動作混接的時間
 
-        symbolSpine.state.addListener({ complete: function() { } })
-        /*symbolSpine.state.queue.complete = () => {
+        symbolSpine.state.addListener({
+            start: function (track) {
+                console.log(track.trackIndex + ' ' + track.animation.name + " Start")
+            },
+            complete: function (track) {
+                console.log(track.trackIndex + ' ' + track.animation.name + " complete")
+            },
+            interrupt: function (track) {
+                console.log(track.trackIndex + ' ' + track.animation.name + " interrupt")
+            },
+            end: function (track) {
+                console.log(track.trackIndex + ' ' + track.animation.name + " end")
+            },
+            event: function (track, event) {
+                console.log(track.trackIndex + ' ' + event.data.name + " event")
+            }
+        });
+        
+    /*symbolSpine.state.addListener({
+        start: function (track) { console.log(track.trackIndex + " Start") },
+        complete: function (track) { console.log(track.trackIndex + " complete") },
+        interrupt: function (track) { console.log(track.trackIndex + " interrupt") },
+        end: function (track) { console.log(track.trackIndex + " end") },
+        event: function (track, event) { console.log(event.data.name + " event") }
+    });*/
 
-            console.log("COMPLETE COMPLETE COMPLETE COMPLETE COMPLETE COMPLETE ")
+
+        /*symbolSpine.state.queue.complete = (track) => {
+
+            console.log(track+ "COMPLETE COMPLETE COMPLETE COMPLETE COMPLETE COMPLETE ")
             //console.log(symbolSpine.state.getCurrent(1).getAnimationTime());
             if (symbolSpine.state.tracks[0] === null) return;
             console.log(symbolSpine.state.tracks[0].getAnimationTime());
@@ -93,14 +151,22 @@ window.onload = function () {
         symbolSpine.state.queue.dispose = () => {
             console.log("Dispose Dispose Dispose Dispose Dispose Dispose Dispose")
         }*/
-        
+
         // 設置空洞化可以緩慢起跑
-        symbolSpine.state.setEmptyAnimation(0, 0);
+        symbolSpine.state.setEmptyAnimation(0, 1.5);
+
         let entry = symbolSpine.state.addAnimation(0, 'run', true, 0);
+        console.log(entry.animation.name);
+        entry.alpha = 0.5;
+        entry.timeScale = 2;
         entry.mixDuration = 1.5;
-        
-        console.log(entry);
-        console.log(symbolSpine.state);
+        console.log(entry.trackIndex);
+
+
+        console.log('TCL: initial -> entry', entry)
+
+        /*console.log(entry);
+        console.log(symbolSpine.state);*/
 
         //symbolSpine.state.addAnimation(0, 'run-to-idle', true, 0);
 
@@ -153,6 +219,7 @@ window.onload = function () {
 
                 symbolSpine.skeleton.setToSetupPose();
                 symbolSpine.state.clearTrack(0);
+
                 console.log('clearTrack(0)')
             }
 
